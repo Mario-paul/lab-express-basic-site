5f066dda61082643a97e0b471309964f255a6c72
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-07-18 00:13:33 -0400

    add comment on how to start node runtime for development

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ app.get('/about', (request, response, next) => response.sendFile(__dirname + '/v
 app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
 
 // Server started
-app.listen(3000, () => console.log('My first app listening on port 3000! '));
+app.listen(process.env.PORT || 3000, () => console.log('My first app listening on port 3000! '));
+
+// To start development server (node live server), enter the following command in the terminal:
+// $ node app.js
+// Then open the website on the browser on localhost:3000 (or whichever port the console prints).
+
+//OR even better, use the
+// $ nodemon
+// command to start the nodemon utility / monitor script
